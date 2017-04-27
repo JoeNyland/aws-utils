ff320bdcaffba451a3c7d9a7a78cc55e1adb4298
Joe Nyland <joenyland@me.com>
2017-04-27 21:17:59 +0100

    Rebuild exe/update-public-ip-in-route53

---
+++ lib/aws/ec2/instance.rb
@@ -0,0 +1,18 @@
+# require 'net/http'
+# require 'open-uri'
+
+module Aws
+  module EC2
+    class Instance
+      METADATA_ENDPOINT = 'http://169.254.169.254/latest/meta-data'.freeze
+
+      def self.local(*args)
+        new(local_instance_id, args)
+      end
+
+      def self.local_instance_id
+        Net::HTTP.get URI.parse("#{METADATA_ENDPOINT}/instance-id")
+      end
+    end
+  end
+end
