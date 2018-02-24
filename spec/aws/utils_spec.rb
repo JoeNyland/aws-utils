60a87b8cc1cd2f57ea43b9de039942c56b663e83
Joe Nyland <joenyland@me.com>
2018-02-24 17:09:05 +0000

    Remove useless test

--- spec/aws/utils_spec.rb
+++ spec/aws/utils_spec.rb
@@ -4,8 +4,4 @@ describe Aws::Utils do
   it 'has a version number' do
     expect(Aws::Utils::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
